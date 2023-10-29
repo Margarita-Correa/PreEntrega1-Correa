85686becf33517560e069add5e87bb45f8fe8db7
Barbara Correa <marcorrea2@gmail.com>
2023-10-28 21:14:15 -0300

    Inicie mi proyecto con React: Pequeña Alpaca Baby Shop

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const Titulo = ()=>{
+  return(
+    <div>
+        <h1>Pequeña Alpaca</h1>
+        <h2>Baby Shop</h2>
+    </div>
+  )
+}
+
+function App() {
+  console.log('hola')
+  return (
+    <>
+      <Titulo />
+    </>
+  )
+}
+
+export default App
